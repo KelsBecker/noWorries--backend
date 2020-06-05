6a719a9827c63f13d9d9ee501408265f218879ec
Kelsbecker <kelly.lynn.becker@gmail.com>
2020-06-05 17:05:18 -0400

    ngrok added to config folder

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,4 +49,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.hosts << "4e9d4fd97395.ngrok.io"
 end
