b50cd52ab11bf95f2886d62e3d15f8bc80521973
Kelsbecker <kelly.lynn.becker@gmail.com>
2020-06-09 20:54:23 -0400

    create method for favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,4 +5,15 @@ class FavoritesController < ApplicationController
         render json: favorites 
     end
 
+    def create 
+        favorite = Favorite.create(favoriteParams)
+        render json: favorite
+    end
+
+    private
+
+    def favoriteParams
+        params.require(:favorite).permit(:user_id, :location_id)
+    end
+
 end
