ba9d8d19884dc7002513536aae17f270cbb4b4d1
Kelsbecker <kelly.lynn.becker@gmail.com>
2020-06-15 07:27:49 -0400

    added delete method to notes controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -5,14 +5,26 @@ class NotesController < ApplicationController
         render json: notes
     end
 
+    def show 
+        note = Note.find(params[:id])
+        render json: note
+    end 
+
     def create 
-        note = Note.create(note_params)
+        note = Note.create(noteParams)
+        render json: note
+    end
+
+    def destroy 
+        note = Note.find(params[:id])
+        note.destroy
         render json: note
     end
 
     private
 
-    def note_params 
+    def noteParams 
         params.require(:note).permit(:user_id, :favorite_id, :content)
     end
+
 end
